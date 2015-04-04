e562f597a689c88c2e08ec40637120a9ae30bdc3
Jeremy Robson <jeremy.robson@gmail.com>
2015-04-04 18:33:35 -0400

    Create templates.js

---
+++ rts/templates.js
@@ -0,0 +1,45 @@
+var healthcolors = ["rgb(255,0,0)", "rgb(255,255,0)", "rgb(0,255,0)"];
+
+var buildingtypes = {
+    "townhall": {
+        "width": 4,
+        "height": 4
+    },
+    "wall": {
+        "width": 1,
+        "height": 1
+    },
+    "mine": {
+        "width": 2,
+        "height": 2,
+    },
+    "farm": {
+        "width": 2,
+        "height": 3
+    },
+    "barracks": {
+        "width": 3,
+        "height": 3
+    }
+};
+
+var tiletypes = {
+    "water": {
+        "color": "steelblue"
+    },
+    "sand": {
+        "color": "khaki"
+    },
+    "mud": {
+        "color": "darkgoldenrod"
+    },
+    "lightgrass": {
+        "color": "yellowgreen"
+    },
+    "mediumgrass": {
+        "color": "forestgreen"
+    },
+    "darkgrass": {
+        "color": "darkgreen"
+    }
+};
