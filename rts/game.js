bba8f9d05230fcc32337129800aaf2cfee9d4262
Jeremy Robson <jeremy.robson@gmail.com>
2015-04-04 16:49:59 -0400

    Update game.js

--- rts/game.js
+++ rts/game.js
@@ -3,8 +3,12 @@ var Game = function(minimapcontext) {
     this.minimap = new MiniMap(this.map, minimapcontext);
 };
 
-Game.prototype.loop = function(mx, my) {
-    this.map.loop(mx, my);
+Game.prototype.loop = function(mx, my, pressed) {
+    this.map.loop(mx, my, pressed);
+};
+
+Game.prototype.menu_click = function(callback) {
+    callback(this.map);
 };
 
 Game.prototype.mouse_down = function(mx, my, button) {
@@ -19,7 +23,7 @@ Game.prototype.mouse_move = function(mx, my, pressed) {
     this.map.mouse_move(mx, my, pressed);
 };
 
-Game.prototype.draw = function(ctx) {
-    this.map.draw(ctx);
+Game.prototype.draw = function(ctx, mx, my) {
+    this.map.draw(ctx, mx, my);
     this.minimap.render();
 };
