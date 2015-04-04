a4143ca9dcb26306f1c2e220244a7a33894760ca
Jeremy Robson <jeremy.robson@gmail.com>
2015-04-04 18:34:54 -0400

    Update unit.js

--- rts/unit.js
+++ rts/unit.js
@@ -1,5 +1,3 @@
-var healthcolors = ["rgb(255,0,0)", "rgb(255,255,0)", "rgb(0,255,0)"];
-
 var Unit = function(team, x, y) {
     this.team = team;
     this.color = (team == "player") ? "rgb(0,255,255)" : "rgb(255,0,100)";
